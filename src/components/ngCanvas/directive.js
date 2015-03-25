a712ac51b39433e3f2ca323ca992e54b51c6f992
maxjchuang <maxjchuang@tencent.com>
2015-03-25 16:16:20 +0800

    改为ngCanvas，painter三层结构，完成鼠标画线

---
+++ src/components/ngCanvas/directive.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('nodePainter')
+  .directive('ngCanvas', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        data: '='
+      },
+      templateUrl: 'components/ngCanvas/template.html',
+      link: function (scope, elem, attr, ctrl) {
+        var ctx = elem[0].getContext('2d');
+        var key;
+
+        scope.$watch('data', function (newVal, oldVal) {
+          _.each(newVal, function (item, index) {
+            key = Object.keys(item)[0];
+            if (_.isArray(item[key])) {
+              ctx[key].apply(ctx, item[key]);
+            } else {
+              ctx[key] = item[key];
+            }
+          });
+        });
+
+      }
+    };
+  });
--- src/components/painter/directive.js
+++ src/components/painter/directive.js
@@ -10,17 +10,55 @@ angular.module('nodePainter')
       },
       templateUrl: 'components/painter/template.html',
       link: function (scope, elem, attr, ctrl) {
-        var ctx = elem[0].getContext('2d');
-        var key;
 
-        _.each(scope.data, function (item, index) {
-          key = Object.keys(item)[0];
-          if (_.isArray(item[key])) {
-            ctx[key].apply(ctx, item[key]);
-          } else {
-            ctx[key] = item[key];
+        scope.backData = [
+          {fillStyle: '#FF00FF'},
+          {fillRect: [100, 300, 500, 10]}
+        ];
+
+        scope.frontData = [];
+        scope.middleData = [];
+
+        scope.isDrawing = false;
+        var tempFront = [], tempMiddle = [];
+        var line = {};
+
+        scope.mouseMove = function (event) {
+          scope.x = event.offsetX;
+          scope.y = event.offsetY;
+          
+          if (scope.isDrawing) {
+            tempFront = [];
+            tempFront.push({'beginPath': []});
+            tempFront.push({'moveTo': [line.x, line.y]});
+            tempFront.push({'lineTo': [event.offsetX, event.offsetY]});
+            tempFront.push({'stroke': []});
+            tempFront.push({'closePath': []});
+            scope.frontData = tempFront;
+            line.x = event.offsetX;
+            line.y = event.offsetY;
+
+            tempMiddle.push({'lineTo': [event.offsetX, event.offsetY]});
           }
-        });
+        };
+
+        scope.mouseDown = function (event) {
+          scope.isDrawing = true;
+          line.x = event.offsetX;
+          line.y = event.offsetY;
+
+          tempMiddle = [];
+          tempMiddle.push({'beginPath': []});
+          tempMiddle.push({'moveTo': [event.offsetX, event.offsetY]});
+        };
+
+        scope.mouseUp = function (event) {
+          scope.isDrawing = false;
+
+          tempMiddle.push({'stroke': []});
+          tempMiddle.push({'closePath': []});
+          scope.middleData = tempMiddle;
+        };
 
       }
     };
